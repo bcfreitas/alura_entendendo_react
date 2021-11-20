4dbc8301a02716a86c2e0e8cfd9dbd01729e4d16
Bruno Freitas <bcfreitas@gmail.com>
2021-11-20 07:33:42 -0300

    importando imagem SVG

--- src/components/cardNota/cardNota.jsx
+++ src/components/cardNota/cardNota.jsx
@@ -1,11 +1,17 @@
 import React, { Component } from "react";
 import "./estilo.css";
+import deleteSVG from "../../assets/img/delete.svg"
+//alem da forma acima, tb podemos importar como ReactComponent. obs: inicial maiscula
+import {ReactComponent as DeleteSVG2} from "../../assets/img/delete.svg"
 class CardNota extends Component {
   render() {
     return (
       <section className="card-nota">
         <header className="card-nota_cabecalho">
           <h3 className="card-nota_titulo">{this.props.titulo}</h3>
+          <img src={deleteSVG} alt="delete"/>
+          {/*quando importamos como ReactComponent, podemos usar da forma abaixo*/}
+          <DeleteSVG2/>
         </header>
         <p className="card-nota_texto">{this.props.texto}</p>
       </section>
